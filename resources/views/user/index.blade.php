492c9be1b218f4d201e53e7861cdad6e6794ea2d
takamura <res_55_9@yahoo.co.jp>
2023-05-05 06:10:06 +0000

    5/5 1st

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -10,8 +10,12 @@
         <div class="game-grid">
             @foreach ($games as $game)
                 <div class="game-card">
-                    <img src="{{ $game->image_path ? secure_asset('storage/image/'. $game->image_path) : secure_asset('storage/image/no_image.png') }}" alt="{{ $game->title }}" style="width: 350px; height: 350px;">
-                    <h2>{{ $game->title }}</h2>
+                    <a href="{{ route('user.show', ['id' => $game->id]) }}">
+                        <img src="{{ $game->image_path ? secure_asset('storage/image/'. $game->image_path) : secure_asset('storage/image/no_image.png') }}" alt="{{ $game->title }}" style="width: 350px; height: 350px;">
+                    </a>
+                    <a href="{{ route('user.show', ['id' => $game->id]) }}">
+                        <h4>{{ $game->title }}</h4>
+                    </a>
                 </div>
             @endforeach
         </div>
--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -4,16 +4,30 @@
 
 @section('content')
 
-    <div class="container">
-        <h1>{{ $game->title }}</h1>
-        <img src="{{ $game->image_path ? secure_asset('storage/image/'. $game->image_path) : secure_asset('storage/image/no_image.png') }}" alt="{{ $game->title }}" style="width: 200px; height: 200px;">
+    <div class="container_show">
+        <img src="{{ $game->image_path ? secure_asset('storage/image/'. $game->image_path) : secure_asset('storage/image/no_image.png') }}" alt="{{ $game->title }}" style="width: 300px; height: 300px;">
+        <h4>{{ $game->title }}</h4>
         <p>ジャンル: {{ $game->genre ? $game->genre->genre_name : '未設定' }}</p>
         <p>機種: {{ $game->platform ? $game->platform->platform_name : '未設定' }}</p>
         <p>プレイ開始日: {{ $game->start_date }}</p>
         <p>プレイ終了日: {{ $game->end_date }}</p>
-        <p>URL: {{ $game->url }}</p>
-        <p>メモ: {{ $game->memo }}</p>
-    </div>
-    
+        <p>ＵＲＬ: {{ $game->url }}</p>
+        <p id="toggle-button">メモ▼(開)</p>
+        <p id="toggle-content" style="display: none;">{{ $game->memo }}</p>
+        
+        <script>
+            document.getElementById('toggle-button').onclick = function() {
+                var content = document.getElementById('toggle-content');
+                var button = document.getElementById('toggle-button');
+                if (content.style.display === 'none') {
+                    content.style.display = 'block';
+                    button.innerHTML = "メモ▲(閉)"; 
+                } else {
+                    content.style.display = 'none';
+                    button.innerHTML = "メモ▼(開)"; 
+                }
+            };
+        </script>
+    <div/>
 @endsection
 
